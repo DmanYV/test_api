49ca3f51e3a30e2d7b9d5c5a794ea26a40b88458
DmanYV <dima-xxx@mail.ru>
2023-04-24 16:11:44 +0300

    -fix

--- schemas/api.py
+++ schemas/api.py
@@ -1,18 +1,17 @@
 from voluptuous import PREVENT_EXTRA, Schema
 
-
 booking = Schema(
     {
         "firstname": str,
         "lastname": str,
         "totalprice": int,
         "depositpaid": bool,
-        "bookingdates" : {
-                            "checkin" : str,
-                            "checkout" : str
+        "bookingdates": {
+                            "checkin": str,
+                            "checkout": str
+                        },
+        "additionalneeds": str
     },
-    "additionalneeds" : str
-    },
-        extra=PREVENT_EXTRA,
-        required=True
-)
+    extra=PREVENT_EXTRA,
+    required=True
+    )
